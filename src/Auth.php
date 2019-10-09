8968edad3e0b825b581e7d86ca74f64b386b3740
Piepin <piepin7228@gmail.com>
2019-10-09 14:04:25 +0700

    Second commit

--- src/Auth.php
+++ src/Auth.php
@@ -17,29 +17,39 @@ class Auth {
 
     public function Validate($token) {
 
+        // Explode string into variables
         list($data, $iat, $crypt) = explode(".",$token);
 
+        // Decode data part
         $data = base64_decode($data);
         $data = json_decode($data);
         print_r($data);
 
+        // Is client secret correct;
         $pass = ($data->client_secret === md5("rahasia".$data->client_id));
 
+        // Decode time
         $iat = base64_decode($iat);
         $iat = json_decode($iat);
         print_r($iat);
 
+        print_r($iat->iat);
+
+        // Get key from decoded data part
         $key = $data->key; 
 
+        // Token based on data
         $compare = md5(base64_decode($key).base64_encode(json_encode($data)).base64_encode(json_encode($iat)));
-
-        return ($compare === $token);
-
-        /*      
+             
         print "COMPARE = $compare<br>";
+        print "TOKEN   = $token<br>";
+
+        /*
         print "TOKEN   = $crypt<br>";
         print "PASS    = $pass<br>";
-        */        
-    }
+        */      
 
+        // Retun compare result
+        return ($compare === $token);
+    }
 }
