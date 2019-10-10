e9be950435fb4aaf5ca199126b27901c8efe9b41
Piepin <piepin7228@gmail.com>
2019-10-10 14:24:23 +0700

    Many updates

--- src/Auth.php
+++ src/Auth.php
@@ -4,14 +4,23 @@ namespace Piepin\Auth;
  
 class Auth {
 
-    private $headers = array();
+    protected $headers = array();
+    protected $debug = false;
+    protected $key   = "*secret-key*";
  
     public function Create($data){
 
         $iat = array("iat" => time());
-        $key = $data["key"]; 
+        // $data["client_secret"] = md5("*secret-key*".$data["client_id"]); 
+        //$data["client_secret"] = $this->CreateSecret($data["client_id"]); 
+        
+        
+        //$key = $data["key"]; 
 
-        $crypt = md5(base64_decode($key).base64_encode(json_encode($data)).base64_encode(json_encode($iat)));
+        //$crypt = md5(base64_decode($key).base64_encode(json_encode($data)).base64_encode(json_encode($iat)));
+        
+        $crypt = md5(base64_encode(json_encode($data)).base64_encode(json_encode($iat)));
+        
         $token = base64_encode(json_encode($data)).".".base64_encode(json_encode($iat)).".".$crypt;
 
         return $token;
@@ -25,56 +34,88 @@ class Auth {
         // Decode data part
         $data = base64_decode($data);
         $data = json_decode($data);
-        print_r($data);
+
+        $this->__debug("DATA", $data);
 
         // Is client secret correct;
-        $pass = ($data->client_secret === md5("rahasia".$data->client_id));
+        $pass = ($data->client_secret === $this->CreateSecret($data->client_id) );
+
+        // Use our formula for comparison
+        $data->client_secret = $this->CreateSecret($data->client_id);
 
         // Decode time
         $iat = base64_decode($iat);
         $iat = json_decode($iat);
-        print_r($iat);
 
         $time = time() < (int) ( $iat->iat + 60);
 
-        print_r($iat->iat);
+        $this->__debug("TIME", $iat->iat);
 
         // Get key from decoded data part
-        $key = $data->key; 
+        // $key = $data->key; 
 
         // Token based on data
-        $compare = md5(base64_decode($key).base64_encode(json_encode($data)).base64_encode(json_encode($iat)));
+        // $compare = md5(base64_decode($key).base64_encode(json_encode($data)).base64_encode(json_encode($iat)));
+        $compare = md5(base64_encode(json_encode($data)).base64_encode(json_encode($iat)));
              
-        print "<br>";
-        print "COMPARE = $compare<br>";
-        print "TOKEN   = ". explode(".",$token)[2] . "<br>";
-        print "PASS    = $pass<br>";
-        print "TIME    = $time<br>";
-
-        /*
-        print "TOKEN   = $crypt<br>";
-        print "PASS    = $pass<br>";
-        */      
+        $this->__debug("USER'S TOKEN", explode(".",$token)[2]);
+        $this->__debug("COMPUTED", $compare);
+        $this->__debug("PASSWORD MATCH", $pass);
+        $this->__debug("TOKEN ISSUED AT", $time);
 
         // Return compare result
         return ($compare === $token);
     }
 
-    public function Read() {
-        print_r($_SERVER);
-
+    public function ReadToken() {
         foreach ($_SERVER as $name => $value) {
             if (substr($name, 0, 5) == 'HTTP_') {
-                $name = ucwords(strtolower(str_replace(["HTTP_","_"],[""," "], $name)));
+                $name = strtolower(str_replace(["HTTP_","_"],[""," "], $name));
+                $name = str_replace(" ","-", $name);
                 $this->headers["$name"] = $value;
             }
         }
-        print_r($this->headers);
-        print $this->GetAuthString();
+       
+        $this->__debug("SERVER", $_SERVER);
+        $this->__debug("AUTH HEADER", $this->GetAuthString());
     }
 
-    private function GetAuthString() {
-        return $this->headers["Auth"];
+    public function GetHeader($key) {
+        $key = strtolower($key);
+        return $this->headers["$key"];
     }
 
+    public function SetDebug($state) {
+        $this->debug = $state;
+    }
+
+    
+    public function CreateSecret($str) {
+        return md5($this->key . $str);
+    }
+    
+    public function ResponseJSON($data, $status=200) {
+        header_remove();
+        header("Content-Type: application/json");
+        header('Status: ' . $status);
+        http_response_code($status);
+        echo json_encode($data);
+        exit();
+    }
+    
+    protected function GetAuthString() {
+        return @$this->headers["auth"];
+    }
+    
+    protected function __debug($label, $var) {
+        if ($this->debug) {
+            print "<pre>";
+            if (is_array($var) || is_object($var)) {
+                print_r($var);
+            } else {
+                print "<strong>$label</strong> = $var<br>";
+            }
+            print "</pre>";
+        }
+    }
 }
