49a8be46a16fc47f2758d883b1a8372bd47beb0e
piepin <piepin7228@gmail.com>
2019-10-11 08:53:52 +0700

    Set a database connection to be able to check the user and password

--- src/Auth.php
+++ src/Auth.php
@@ -4,23 +4,30 @@ namespace Piepin\Auth;
  
 class Auth {
 
-    protected $headers = array();
-    protected $debug = false;
-    protected $key   = "*secret-key*";
+    protected $headers      = array();
+    protected $debug        = false;
+    protected $key          = "*secret-key*";
+    protected $db           = null;
+    protected $user_col     = "username";
+    protected $pass_col     = "password";
+    protected $user_table   = "users";
+
+    public function SetDatabase($pdo) {
+        $this->db = $pdo;
+    }
  
     public function Create($data){
 
+        // Now
         $iat = array("iat" => time());
-        // $data["client_secret"] = md5("*secret-key*".$data["client_id"]); 
-        //$data["client_secret"] = $this->CreateSecret($data["client_id"]); 
-        
+
+        // Encrypt real password
+        $data["client_secret"] = $this->CreateSecret($data["client_id"]); 
         
         //$key = $data["key"]; 
-
         //$crypt = md5(base64_decode($key).base64_encode(json_encode($data)).base64_encode(json_encode($iat)));
         
         $crypt = md5(base64_encode(json_encode($data)).base64_encode(json_encode($iat)));
-        
         $token = base64_encode(json_encode($data)).".".base64_encode(json_encode($iat)).".".$crypt;
 
         return $token;
@@ -32,13 +39,16 @@ class Auth {
         list($data, $iat, $crypt) = explode(".",$token);
 
         // Decode data part
+        $this->__debug("DATA", $data);
         $data = base64_decode($data);
         $data = json_decode($data);
 
-        $this->__debug("DATA", $data);
+        $this->__debug("DATA DECODED", $data);
 
-        // Is client secret correct;
-        $pass = ($data->client_secret === $this->CreateSecret($data->client_id) );
+        //=== Is client secret correct?
+        if ( $data->client_secret !== $this->CreateSecret($data->client_id) ) {
+            //return false;
+        }
 
         // Use our formula for comparison
         $data->client_secret = $this->CreateSecret($data->client_id);
@@ -46,25 +56,45 @@ class Auth {
         // Decode time
         $iat = base64_decode($iat);
         $iat = json_decode($iat);
-
-        $time = time() < (int) ( $iat->iat + 60);
-
         $this->__debug("TIME", $iat->iat);
+        
+        //=== Is time expired?
+        if ( time() >= (int) ( $iat->iat + 60) ) {
+            //return false;
+        }
 
-        // Get key from decoded data part
-        // $key = $data->key; 
-
-        // Token based on data
-        // $compare = md5(base64_decode($key).base64_encode(json_encode($data)).base64_encode(json_encode($iat)));
+        //=== Compare token
         $compare = md5(base64_encode(json_encode($data)).base64_encode(json_encode($iat)));
-             
         $this->__debug("USER'S TOKEN", explode(".",$token)[2]);
-        $this->__debug("COMPUTED", $compare);
-        $this->__debug("PASSWORD MATCH", $pass);
-        $this->__debug("TOKEN ISSUED AT", $time);
+        $this->__debug("COMPUTED    ", $compare);
+        // $this->__debug("PASSWORD MATCH", $pass);
+        // $this->__debug("TOKEN ISSUED AT", $time);
+        if ($compare !== $token) {
+            //return false;
+        }
 
-        // Return compare result
-        return ($compare === $token);
+        //=== Check user and password in PDO database
+        try {
+            if ($this->db != null) {
+                $sql = "SELECT COUNT(*) AS num FROM user WHERE username = :user and password = :pass";
+                $stmt = $this->db->prepare($sql);
+                $stmt->bindValue(':user', $data->client_id, \PDO::PARAM_STR);
+                $stmt->bindValue(':pass', $data->client_secret, \PDO::PARAM_STR);
+                $stmt->execute();
+                $row = $stmt->fetch(\PDO::FETCH_ASSOC);
+                if($row['num'] <= 0) {
+                    //return false;
+                } 
+            } else {
+                throw new \Exception("The database has not been set.");
+            }
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            die();
+        }
+
+        //=== Every validation steps is passed, return true
+        return true;
     }
 
     public function ReadToken() {
@@ -75,7 +105,6 @@ class Auth {
                 $this->headers["$name"] = $value;
             }
         }
-       
         $this->__debug("SERVER", $_SERVER);
         $this->__debug("AUTH HEADER", $this->GetAuthString());
     }
@@ -88,7 +117,6 @@ class Auth {
     public function SetDebug($state) {
         $this->debug = $state;
     }
-
     
     public function CreateSecret($str) {
         return md5($this->key . $str);
@@ -101,6 +129,12 @@ class Auth {
         http_response_code($status);
         echo json_encode($data);
         exit();
+    }
+
+    public function SetUserTable($table, $user, $pass) {
+        $this->user_table = $table;
+        $this->user_col   = $user;
+        $this->pass_col   = $pass;
     }
     
     protected function GetAuthString() {
