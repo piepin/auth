fa84f4184b5c5b2cad83fed070b02635d3a83117
piepin <piepin7228@gmail.com>
2019-10-11 13:20:20 +0700

    Read post variables

--- src/Auth.php
+++ src/Auth.php
@@ -1,39 +1,111 @@
 <?php
+/**
+ * Simple Authorization class with PHP
+ *
+ * 1. CLIENT --->[send user + pass]---> SERVER
+ *    Client post user + password to server  
+ * 
+ * 2. CLIENT <---[token           ]<--- SERVER
+ *    If password or token is correct, 
+ *    server will create token and send response to client
+ *  
+ * 3. CLIENT --->[token + request ]---> SERVER
+ *    Client post request data + token to server
+ * 
+ * 4. CLIENT <---[response        ]<--- SERVER
+ *    If token and password is correct and not expired,
+ *    server will send asked data in JSON format 
+ *
+ * @version    Release: @package_version@
+ * @author     Piepin <piepin7228@gmail.com>
+ * @license    https://opensource.org/licenses/MIT MIT License
+ * @link       https://github.com/piepin/auth
+ * @since      File available since Release 1.0.0
+ */ 
 
 namespace Piepin\Auth;
  
 class Auth {
 
+    /**
+     * Encryption key, please change
+     * @var string $headers
+     */
+    protected $key          = "*~/secret^&^key/~*";
+    
+    /**
+     * Holds the header from the client
+     * @var string $headers
+     */
     protected $headers      = array();
+
+    /**
+     * Dump variables when debug is set to true
+     * @var bool $debug
+     */
     protected $debug        = false;
-    protected $key          = "*secret-key*";
-    protected $db           = null;
+    
+    /**
+     * Holds database object
+     * @var object $db
+     */
+    protected $db;
+
+    /**
+     * Holds table's name and field's name
+     * @var string $user_table
+     * @var string $user_col
+     * @var string $pass_col
+     */
     protected $user_col     = "username";
     protected $pass_col     = "password";
     protected $user_table   = "users";
 
+    /**
+     * Set database object (PDO)
+     *
+     * Set database object (PDO) for user and password validation.
+     *
+     * @param object $pdo PDO database object
+     * @return bool  Return true is parameter is object
+     *
+     * @access public
+     */
     public function SetDatabase($pdo) {
         $this->db = $pdo;
+        return (is_object($pdo));
     }
- 
-    public function Create($data){
 
-        // Now
+    /**
+     * Create security token 
+     *
+     * Create simple token based on my own algorithm :)  
+     *  
+     * Parameter format: 
+     * $data = array( "client_id" => "myusername",
+     *                "client_secret" => $auth->CreateSecret("mypassword") );
+     *
+     * @param array   $data Login data 
+     * @return string Token created
+     *
+     * @access public
+     */
+     public function CreateToken($data){
+
+        // Issued at now
         $iat = array("iat" => time());
 
         // Encrypt real password
         $data["client_secret"] = $this->CreateSecret($data["client_id"]); 
-        
-        //$key = $data["key"]; 
-        //$crypt = md5(base64_decode($key).base64_encode(json_encode($data)).base64_encode(json_encode($iat)));
-        
+
+        // Create token
         $crypt = md5(base64_encode(json_encode($data)).base64_encode(json_encode($iat)));
         $token = base64_encode(json_encode($data)).".".base64_encode(json_encode($iat)).".".$crypt;
 
         return $token;
     }
 
-    public function Validate($token) {
+    public function ValidateToken($token) {
 
         // Explode string into variables
         list($data, $iat, $crypt) = explode(".",$token);
@@ -42,12 +114,13 @@ class Auth {
         $this->__debug("DATA", $data);
         $data = base64_decode($data);
         $data = json_decode($data);
-
         $this->__debug("DATA DECODED", $data);
 
         //=== Is client secret correct?
         if ( $data->client_secret !== $this->CreateSecret($data->client_id) ) {
-            //return false;
+            if (!$this->debug) {
+                return false;
+            }
         }
 
         // Use our formula for comparison
@@ -60,7 +133,9 @@ class Auth {
         
         //=== Is time expired?
         if ( time() >= (int) ( $iat->iat + 60) ) {
-            //return false;
+            if (!$this->debug) {
+                return false;
+            }
         }
 
         //=== Compare token
@@ -70,7 +145,9 @@ class Auth {
         // $this->__debug("PASSWORD MATCH", $pass);
         // $this->__debug("TOKEN ISSUED AT", $time);
         if ($compare !== $token) {
-            //return false;
+            if (!$this->debug) {
+                return false;
+            }
         }
 
         //=== Check user and password in PDO database
@@ -83,7 +160,9 @@ class Auth {
                 $stmt->execute();
                 $row = $stmt->fetch(\PDO::FETCH_ASSOC);
                 if($row['num'] <= 0) {
-                    //return false;
+                    if (!$this->debug) {
+                        return false;
+                    }
                 } 
             } else {
                 throw new \Exception("The database has not been set.");
@@ -107,6 +186,13 @@ class Auth {
         }
         $this->__debug("SERVER", $_SERVER);
         $this->__debug("AUTH HEADER", $this->GetAuthString());
+        return $this->GetAuthString();
+    }
+
+    public function ReadPost() {
+        $raw = file_get_contents('php://input');
+        parse_str($raw, $post);
+        return $post;
     }
 
     public function GetHeader($key) {
@@ -123,12 +209,19 @@ class Auth {
     }
     
     public function ResponseJSON($data, $status=200) {
-        header_remove();
-        header("Content-Type: application/json");
-        header('Status: ' . $status);
-        http_response_code($status);
-        echo json_encode($data);
-        exit();
+        if (!headers_sent()) {
+            header_remove();
+            header("Server: West Java");
+            header("Content-Type: application/json");
+            header('Status: ' . $status);
+            http_response_code($status);
+            echo json_encode($data);
+            die();
+        } else {
+            $this->__debug("ERROR", "Header already sent.");
+            $this->__debug("HEADER", headers_list());
+            die();
+        }
     }
 
     public function SetUserTable($table, $user, $pass) {
@@ -153,3 +246,11 @@ class Auth {
         }
     }
 }
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
